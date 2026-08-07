2339ec8fc5646805712c5c058a6c761673f93bc4
Ashutoshkumarpandey176 <ashutoshkumarpandey212@gmail.com>
2026-08-07 21:38:05 +0530

    Time: 4 ms (12.12%) | Memory: 26 MB (23.77%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -3,10 +3,8 @@ public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n);
-
         int low = 0;  
         int high = 1;  
-
         for (int i = 0; i < n; i++) {
             if (nums[i] % 2 == 0) {
                 ans[low] = nums[i];
