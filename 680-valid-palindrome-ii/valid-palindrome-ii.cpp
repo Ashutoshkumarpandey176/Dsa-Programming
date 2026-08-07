c843b2c30a34a6fb5cdd7b89f80eebf95342d8f3
Ashutoshkumarpandey176 <ashutoshkumarpandey212@gmail.com>
2026-08-07 13:33:30 +0530

    Time: 4 ms (56.01%) | Memory: 21.9 MB (93.06%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+bool check(string &s,int low,int high){
+    while(low<high){
+        if(s[low]!=s[high])
+        {
+            return false;
+        }
+        low++;
+        high--;  
+    }
+    return true;
+}
+    bool validPalindrome(string s) {
+       int low=0;
+       int high=s.size()-1;
+       while(low<high)
+       {
+          if(s[low]==s[high])
+          {
+             low++;
+             high--;
+          }
+          else
+          {
+            return check(s,low+1,high) || check(s,low,high-1);
+          }
+       }
+       return true;
+    }
+};
