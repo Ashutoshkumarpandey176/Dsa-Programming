da6d67a70d6739c3c324c1e75bfee261f89ecd1d
Ashutoshkumarpandey176 <ashutoshkumarpandey212@gmail.com>
2026-08-09 20:15:19 +0530

    Time: 3 ms (45.95%) | Memory: 26.4 MB (37.03%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> freq(nums.size() + 1, 0);
-        for(int i=0;i<nums.size();i++){
+        int n = nums.size();
+
+        vector<int> freq(n + 1, 0);
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
             freq[nums[i]]++;
         }
-        int duplicate=0;
-        int num=0;
-        for(int i=1;i<=nums.size();i++){
-            if(freq[i]==2){
-                duplicate=i;
+
+        int duplicate = 0;
+        int notfound = 0;
+
+        for (int i = 1; i <= nums.size(); i++) {
+            if (freq[i] == 2) {
+                duplicate = i;
             }
-            if(freq[i]==0)
-            {
-                num=i;
+            else if (freq[i] == 0) {
+                notfound = i;
             }
         }
-        return {duplicate,num};
+
+        ans.push_back(duplicate);
+        ans.push_back(notfound);
+
+        return ans;
     }
 };
