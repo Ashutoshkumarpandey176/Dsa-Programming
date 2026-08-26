75dfd8ae2bfcfd11cf2cb42f38c164bc24881d3a
Ashutoshkumarpandey176 <ashutoshkumarpandey212@gmail.com>
2026-08-26 16:22:03 +0530

    Time: 19 ms (14.43%) | Memory: 18.9 MB (11.61%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -7,17 +7,14 @@ public:
         {
             return true;
         }
-
         if(dp[idx] != -1)
         {
             return dp[idx];
         }
-
         for(int i = idx; i < s.size(); i++)
         {
             string temp = s.substr(idx, i - idx + 1);
-
-            if(wordDict.find(temp) != wordDict.end())
+            if(wordDict.count(temp))
             {
                 if(solve(s, wordDict, i + 1))
                 {
@@ -25,16 +22,12 @@ public:
                 }
             }
         }
-
         return dp[idx] = false;
     }
-
     bool wordBreak(string s, vector<string>& wordDict)
     {
         unordered_set<string> words(wordDict.begin(), wordDict.end());
-
         memset(dp,-1,sizeof(dp));
-
         return solve(s, words, 0);
     }
 };
