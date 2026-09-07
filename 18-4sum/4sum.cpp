416484fe7e64004eb7b2dbf4fb4114b375c80bee
Ashutoshkumarpandey176 <ashutoshkumarpandey212@gmail.com>
2026-09-08 00:14:38 +0530

    Time: 181 ms (7.71%) | Memory: 53.6 MB (6.93%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        set<vector<int>>st;
+     sort(nums.begin(),nums.end());
+       set<vector<int>>s;
         vector<vector<int>>ans;
         for(int i=0;i<nums.size();i++)
         {
             for(int j=i+1;j<nums.size();j++)
             {
-                int low=j+1;
-                int high=nums.size()-1;
-                while(low<high)
-                {
-                long long sum=(long long)nums[i]+nums[j]+nums[low]+nums[high];
-                if(sum==target)
-                {
-                    st.insert({nums[i], nums[j], nums[low], nums[high]});
-                    low++;
-                    high--;
-                }
-              else  if(sum<target)
-                {
-                    low++;
-                }
-                else
-                {
-                    high--;
-                }
-                }
+                 int left=j+1;
+                 int right=nums.size()-1;
+                 long long sum=0;
+                 while(left<right)
+                 {
+                    sum=(long) (long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target)
+                    {
+                        s.insert({nums[i],nums[j],nums[left],nums[right]});
+                        left++;
+                        right--;
+                    }
+                    else if(sum<target)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                 }
+
             }
+
         }
-        for(auto x:st)
+        for(auto x:s)
         {
             ans.push_back(x);
         }
         return ans;
-
     }
 };
+               
