acccc132c7d4d4cccec3f36827419bfe7d6dbd9d
Ashutoshkumarpandey176 <ashutoshkumarpandey212@gmail.com>
2026-09-13 22:13:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.95%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        
+        int i = 0;
+        int j = 0;
+
+        while (i < version1.size() || j < version2.size()) {
+            
+            long long num1 = 0;
+            long long num2 = 0;
+
+            // Extract one revision from version1
+            while (i < version1.size() && version1[i] != '.') {
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
+            }
+
+            // Extract one revision from version2
+            while (j < version2.size() && version2[j] != '.') {
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+
+            // Compare current revisions
+            if (num1 < num2)
+                return -1;
+
+            if (num1 > num2)
+                return 1;
+
+            // Move past '.'
+            if (i < version1.size())
+                i++;
+
+            if (j < version2.size())
+                j++;
+        }
+
+        return 0;
+    }
+};
