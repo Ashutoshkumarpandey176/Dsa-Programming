c4f54bcef9861274421d313ea1efb3007ee8b198
Ashutoshkumarpandey176 <ashutoshkumarpandey212@gmail.com>
2026-09-16 13:03:06 +0530

    Time: 4 ms (74.07%) | Memory: 17.1 MB (94.58%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int low = 0;
-        int high = low + 1;
-        long long count = 0;
-
-        while (high != s.size()) {
-            if (s[low] == '1' && s[high] == '0') {
-                int temp = low;
-
-                while (temp >= 0 && s[temp] == '1') {
-                    swap(s[temp], s[high]);
-                    count += high - temp;
-                    high++;
-                    temp--;
-                }
-
-                low++;
+        int n = s.length();
+        long long swap=0;
+        int black=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='0')
+            {
+                swap=swap+black;
             }
-            else if (s[low] == '0') {
-                low++;
-                high++;
-            }
-            else if (s[low] == '1' && s[high] == '1') {
-                high++;
+            else
+            {
+                black++;
             }
         }
-
-        return count;
+        return swap;
     }
 };
